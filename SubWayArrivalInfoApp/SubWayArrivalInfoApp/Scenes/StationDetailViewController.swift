7b0363ec65d627449a0545a4149629f2b4bc4938
Caumannerman <uigohiiming2@gmail.com>
2022-04-03 22:19:09 +0900

    Push DetailViewController when tableView cell clicked

---
+++ SubWayArrivalInfoApp/SubWayArrivalInfoApp/Scenes/StationDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StationDetailViewController.swift
+//  SubWayArrivalInfoApp
+//
+//  Created by 양준식 on 2022/04/03.
+//
+
+import UIKit
+import SnapKit
+
+
+final class StationDetailViewController: UIViewController{
+    
+}
