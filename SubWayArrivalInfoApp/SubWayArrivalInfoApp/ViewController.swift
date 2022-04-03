4d7c7dcb1704af86c735b5f865e4b0c00609c457
Caumannerman <uigohiiming2@gmail.com>
2022-04-03 17:49:27 +0900

    Start Subway Arrival Time Information App without storyboard

---
+++ SubWayArrivalInfoApp/SubWayArrivalInfoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubWayArrivalInfoApp
+//
+//  Created by 양준식 on 2022/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
