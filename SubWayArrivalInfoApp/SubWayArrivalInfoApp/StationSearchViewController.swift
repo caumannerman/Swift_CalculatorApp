e5cde3fdb6aaeda7f5e552bc69559b710a9c4bd3
Caumannerman <uigohiiming2@gmail.com>
2022-04-03 22:02:06 +0900

    Add TableView on ViewController under UISearchBar

--- SubWayArrivalInfoApp/SubWayArrivalInfoApp/StationSearchViewController.swift
+++ SubWayArrivalInfoApp/SubWayArrivalInfoApp/StationSearchViewController.swift
@@ -10,11 +10,22 @@ import SnapKit
 
 class StationSearchViewController: UIViewController {
     
-    private lazy tableView:
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        setNavigationItems()
+        setTableViewLayout()
+    }
+    
+    private func setNavigationItems(){
         
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "지하철 도착 정보"
@@ -24,10 +35,31 @@ class StationSearchViewController: UIViewController {
         searchController.searchBar.placeholder = "지하철 역을 입력해주세요"
         searchController.obscuresBackgroundDuringPresentation = false
         
+        //navigation에 부착되는 것이다.
         navigationItem.searchController = searchController
         
+    }
+    
+    private func setTableViewLayout() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
     }
 
 
 }
 
+extension StationSearchViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.item)"
+        
+        return cell
+    }
+}
