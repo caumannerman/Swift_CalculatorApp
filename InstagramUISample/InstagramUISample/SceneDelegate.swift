f650695d88ede2cd2a7915b01e0032c69b7769d8
Caumannerman <uigohiiming2@gmail.com>
2022-04-04 20:52:29 +0900

    Set tabBarItem & app tintColor on SceneDelegate

--- InstagramUISample/InstagramUISample/SceneDelegate.swift
+++ InstagramUISample/InstagramUISample/SceneDelegate.swift
@@ -19,6 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .systemBackground
         window?.rootViewController = TabBarController()
+        window?.tintColor = .label //다크모드 여부에 따라 흑 or 백
         window?.makeKeyAndVisible()
     }
 
