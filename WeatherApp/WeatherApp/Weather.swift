52cf0851a236ed1e376a7eecffc38812e616f2fe
Caumannerman <uigohiiming2@gmail.com>
2022-04-18 23:26:07 +0900

    통신을 위한 WeatherInformation 구조체(dto) 구현 with CodingKeys

--- WeatherApp/WeatherApp/Weather.swift
+++ WeatherApp/WeatherApp/Weather.swift
@@ -8,5 +8,33 @@
 import Foundation
 
 struct WeatherInformation: Codable {
+    let weather: [Weather]?
+    let temp: Temp?
+    let name: String?
     
+    enum CodingKeys: String, CodingKey{
+        case weather
+        case temp = "main"
+        case name
+    }
+    
+}
+
+struct Weather: Codable{
+    
+    let id: Int?
+    let main, description, icon: String?
+  
+    
+}
+
+struct Temp: Codable{
+    let temp, feelsLike, minTemp, maxTemp: Double?
+    
+    enum CodingKeys: String, CodingKey{
+        case temp
+        case feelsLike = "feels_like"
+        case minTemp = "temp_min"
+        case maxTemp = "temp_max"
+    }
 }
